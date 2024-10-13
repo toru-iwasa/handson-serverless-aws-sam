9347cd53bec121303ad61d27734f6b2648a181d9
toru.iwasa <toru.iwasa@beex-inc.com>
2024-10-13 22:17:20 +0900

    API Gatewayとlambdaの連携まで

---
+++ translate-function/translate-function.py
@@ -0,0 +1,25 @@
+import json
+import boto3
+
+translate = boto3.client(service_name='translate')
+
+def lambda_handler(event, context):
+
+    input_text = event['queryStringParameters']['input_text']
+
+    response = translate.translate_text(
+        Text=input_text,
+        SourceLanguageCode='ja',
+        TargetLanguageCode='en'
+    )
+
+    output_text = response.get('TranslatedText')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'output_text': output_text
+        }),
+        'isBase64Encoded': False,
+        'headers': {}
+    }
